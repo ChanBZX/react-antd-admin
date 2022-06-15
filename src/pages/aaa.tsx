b5f634726612615bbfe4293d30a09b6d601072d9
VEZON <wl885683@163.com>
2022-06-16 07:19:02 +0800

    feat: axios

--- src/pages/aaa.tsx
+++ src/pages/aaa.tsx
@@ -1,7 +1,18 @@
+import { login} from '@/services/index'
+
 function Caaa() {
+  const sadsasd =async ()=>{
+    const res = await login({
+      username: 'admin',
+      password: 'admin'
+    })
+    console.log(res);
+  }
+
+
   return (
     <div className="App">
-      appasdadads
+      <div onClick={sadsasd}>接口</div>
     </div>
   )
 }
